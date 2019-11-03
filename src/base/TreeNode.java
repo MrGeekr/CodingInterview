5f120529c14d92dfdfc075317eadf94f69a23b60
MrGeekr <939220189@qq.com>
2019-11-03 22:36:20 +0800

    二叉树的结点

---
+++ src/base/TreeNode.java
@@ -0,0 +1,14 @@
+package base;
+
+/**
+ * Created by MrGeekr.
+ * Date : 2019/9/30.
+ * Description :二叉树的结点
+ */
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int x) { val = x; }
+
+}
