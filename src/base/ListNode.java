6c5820aeb717c769d96aa48f303ccd6a3545637f
MrGeekr <939220189@qq.com>
2019-11-03 22:35:19 +0800

    链表结点

---
+++ src/base/ListNode.java
@@ -0,0 +1,15 @@
+package base;
+
+/**
+ * Created by MrGeekr.
+ * Date : 2019/9/30.
+ * Description :链表结点
+ */
+public class ListNode {
+
+    public int val;
+    public ListNode next = null;
+    ListNode(int val) {
+        this.val = val;
+    }
+}
