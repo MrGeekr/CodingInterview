f4ba36b3ce9158500bf81bb2f90db1edb44c7a98
MrGeekr <939220189@qq.com>
2019-11-03 22:35:31 +0800

    复制链表的结点

---
+++ src/base/RandomListNode.java
@@ -0,0 +1,16 @@
+package base;
+
+/**
+ * Created by MrGeekr.
+ * Date : 2019/10/5.
+ * Description :复制链表的结点
+ */
+public class RandomListNode {
+    int label;
+    RandomListNode next = null;
+    RandomListNode random = null;
+
+    RandomListNode(int label) {
+        this.label = label;
+    }
+}
